967ea858934fc468a37a0df7fbef6a501651b0f2
Carlos Daniel <carlosporrasayala@gmail.com>
2024-07-19 18:52:21 -0500

    Add Swipe interaction on expenses to edit and delete

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -1,8 +1,17 @@
 import { useMemo } from 'react';
+import {
+  LeadingActions,
+  SwipeableList,
+  SwipeableListItem,
+  SwipeAction,
+  TrailingActions,
+} from 'react-swipeable-list';
+
 import { formatDate } from '../helpers';
 import { Expense } from '../types';
 import AmountDisplay from './AmountDisplay';
 import { categories } from '../data/categories';
+import 'react-swipeable-list/dist/styles.css';
 
 type ExpenseDetailProps = {
   expense: Expense;
@@ -14,25 +23,61 @@ export default function ExpenseDetail({ expense }: ExpenseDetailProps) {
     [expense]
   );
 
+  const leadingActions = () => {
+    return (
+      <LeadingActions>
+        <SwipeAction
+          onClick={() => {
+            console.log('Leading action clicked');
+          }}
+        >
+          Actualizar
+        </SwipeAction>
+      </LeadingActions>
+    );
+  };
+
+  const trailingActions = () => {
+    return (
+      <TrailingActions>
+        <SwipeAction
+          destructive={true}
+          onClick={() => {
+            console.log('Trailing action clicked');
+          }}
+        >
+          Eliminar
+        </SwipeAction>
+      </TrailingActions>
+    );
+  };
+
   return (
-    <div className='bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center'>
-      <div>
-        <img
-          src={`/img/icono_${categoryInfo!.icon}.svg`}
-          alt='Icono de gasto'
-          className='w-20'
-        />
-      </div>
-      <div className='flex-1 space-y-2'>
-        <p className='text-sm font-bold uppercase text-slate-500'>
-          {categoryInfo!.name}
-        </p>
-        <p>{expense.expenseName}</p>
-        <p className='text-slate-600 text-sm'>
-          {formatDate(expense.date?.toString())}
-        </p>
-      </div>
-      <AmountDisplay amount={expense.amount} />
-    </div>
+    <SwipeableList>
+      <SwipeableListItem
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()}
+      >
+        <div className='bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center'>
+          <div>
+            <img
+              src={`/img/icono_${categoryInfo!.icon}.svg`}
+              alt='Icono de gasto'
+              className='w-20'
+            />
+          </div>
+          <div className='flex-1 space-y-2'>
+            <p className='text-sm font-bold uppercase text-slate-500'>
+              {categoryInfo!.name}
+            </p>
+            <p>{expense.expenseName}</p>
+            <p className='text-slate-600 text-sm'>
+              {formatDate(expense.date?.toString())}
+            </p>
+          </div>
+          <AmountDisplay amount={expense.amount} />
+        </div>
+      </SwipeableListItem>
+    </SwipeableList>
   );
 }
